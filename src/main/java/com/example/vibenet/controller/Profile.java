09c24b7f8ed805a025f3d1ec9b77362e8c415986
Tregyn <tregyn@gmail.com>
2024-05-03 03:14:47 +0800

    Добавлен Личный Кабинет

--- src/main/java/com/example/vibenet/controller/Profile.java
+++ src/main/java/com/example/vibenet/controller/Profile.java
@@ -1,35 +1,26 @@
 package com.example.vibenet.controller;
 
 import com.example.vibenet.entity.User;
-import com.example.vibenet.entity.User.*;
-import com.example.vibenet.repository.UserRepository;
 import com.example.vibenet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Base64;
-import java.util.Map;
-import java.util.Optional;
 
 
 @Controller
-public class MainPage {
+public class Profile {
 
     private final UserService userService;
 
     @Autowired
-    public MainPage(UserService userService) {
+    public Profile(UserService userService) {
         this.userService = userService;
     }
 
@@ -54,13 +45,13 @@ public class MainPage {
         return null; // Или возвращайте данные изображения по умолчанию
     }
 
-    @GetMapping("/")
-    public String mainpage(@AuthenticationPrincipal OAuth2User principal, Model model) {
+    @GetMapping("/profile")
+    public String profile(@AuthenticationPrincipal OAuth2User principal, Model model) {
         // Можно добавить атрибуты модели, которые будут использоваться в шаблоне
         model.addAttribute("username", currentUser(principal).getUsername());
 //        model.addAttribute("avatar", getProfilePicture(currentUser(principal)));
         model.addAttribute("avatar", getProfilePictureAsBase64(currentUser(principal)));
-        return "index"; // Возвращает имя шаблона без расширения .html
+        return "profile"; // Возвращает имя шаблона без расширения .html
     }
 
 }
