36b132dca379842187c3c63110f2ba0e13788d03
Tregyn <tregyn@gmail.com>
2024-05-09 04:28:26 +0800

    - подготовка к деплою

--- src/main/java/com/example/vibenet/VibeNetApplication.java
+++ src/main/java/com/example/vibenet/VibeNetApplication.java
@@ -1,6 +1,5 @@
 package com.example.vibenet;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VibeNetApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("src/main/resources")
-				.filename("token.env")
-				.load();
-		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-		System.setProperty("BD_USERNAME", dotenv.get("BD_USERNAME"));
-		System.setProperty("BD_PASS", dotenv.get("BD_PASS"));
-
 
 		SpringApplication.run(VibeNetApplication.class, args);
 	}
